cd9343e3c0d145b506e9fdee650cce93048cef11
ErosEros <eros.vittori1994@gmail.com>
2022-10-14 03:56:18 +0200

    create array

--- Snake1.php
+++ Snake1.php
@@ -77,12 +77,6 @@ for($i=0; $i < count($array); $i++){
 
 
 <!-- 
-Snack 3
-Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: 
-DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. 
-Stampare ogni data con i relativi post.
-Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
-
 Snack 4
 Prendere un paragrafo abbastanza lungo, contenente diverse frasi. 
 Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
